0e36f4456aff3b4c85f3fd986c61cc43dade2404
Sami BOUDOUKHA <sboudouk@e3r9p18.42.fr>
2014-11-06 20:04:56 +0100

    libft

---
+++ day01/ft_strcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboudouk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/03 11:28:03 by sboudouk          #+#    #+#             */
+/*   Updated: 2014/11/06 17:53:49 by sboudouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strcmp(const char *s1, const char *s2)
+{
+	unsigned char	*ps1;
+	unsigned char	*ps2;
+
+	ps1 = (unsigned char *)s1;
+	ps2 = (unsigned char *)s2;
+	while (*ps1)
+	{
+		if (*ps1 != *ps2)
+			return ((*ps1 - *ps2));
+		ps1++;
+		ps2++;
+	}
+	return (0);
+}
